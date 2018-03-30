fb39ed1dffab792eed0d9178b8a4a98366fbddb6
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-30 14:11:25 -0400

    Removed unnecessary header includes.

--- src/AudioController.h
+++ src/AudioController.h
@@ -7,30 +7,17 @@
 // ----------------------------------------------------------------------------
 #ifndef AUDIO_CONTROLLER_H
 #define AUDIO_CONTROLLER_H
-#include <Flash.h>
-#include <EEPROM.h>
-#include <Streaming.h>
 #include <ArduinoJson.h>
-#include <JsonSanitizer.h>
 #include <JsonStream.h>
 #include <Array.h>
 #include <Vector.h>
-#include <MemoryFree.h>
 #include <ConstantVariable.h>
-#include <SavedVariable.h>
 #include <Functor.h>
+
 #include <IndexedContainer.h>
-#include <FunctorCallbacks.h>
-#include <Watchdog.h>
-
 #include <EventController.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <SD.h>
-#include <SerialFlash.h>
-#include <Audio.h>
 
-#include <ModularClient.h>
+#include <Audio.h>
 
 #include <ModularServer.h>
 #include <ModularDeviceBase.h>
