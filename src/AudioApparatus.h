a642bc59985442d4c7a32ddf5a97cd44953f5149
Peter Polidoro <peter@polidoro.io>
2022-02-04 14:58:57 -0500

    Remove any function that uses modular_server

--- src/AudioApparatus.h
+++ src/AudioApparatus.h
@@ -19,17 +19,11 @@ public:
 
   SDInterface & getSDInterface();
   bool playPath(const char * path);
-  void playTone(size_t frequency,
-    const ConstantString * const speaker_ptr);
   void playToneAt(size_t frequency,
     const ConstantString * const speaker_ptr,
     long volume);
-  void playNoise(const ConstantString * const speaker_ptr);
   void playNoiseAt(const ConstantString * const speaker_ptr,
     long volume);
-  void playFilteredNoise(size_t frequency,
-    double bandwidth,
-    const ConstantString * const speaker_ptr);
   void playFilteredNoiseAt(size_t frequency,
     double bandwidth,
     const ConstantString * const speaker_ptr,
@@ -43,12 +37,6 @@ public:
   bool codecEnabled();
   bool pathIsAudio(const char * path);
 
-  int addTonePwm(size_t frequency,
-    const ConstantString * const speaker_ptr,
-    long delay,
-    long period,
-    long on_duration,
-    long count);
   int addTonePwmAt(size_t frequency,
     const ConstantString * const speaker_ptr,
     long volume,
@@ -56,44 +44,23 @@ public:
     long period,
     long on_duration,
     long count);
-  int startTonePwm(size_t frequency,
-    const ConstantString * const speaker_ptr,
-    long delay,
-    long period,
-    long on_duration);
   int startTonePwmAt(size_t frequency,
     const ConstantString * const speaker_ptr,
     long volume,
     long delay,
     long period,
     long on_duration);
-  int addNoisePwm(const ConstantString * const speaker_ptr,
-    long delay,
-    long period,
-    long on_duration,
-    long count);
   int addNoisePwmAt(const ConstantString * const speaker_ptr,
     long volume,
     long delay,
     long period,
     long on_duration,
     long count);
-  int startNoisePwm(const ConstantString * const speaker_ptr,
-    long delay,
-    long period,
-    long on_duration);
   int startNoisePwmAt(const ConstantString * const speaker_ptr,
     long volume,
     long delay,
     long period,
     long on_duration);
-  int addFilteredNoisePwm(size_t frequency,
-    double bandwidth,
-    const ConstantString * const speaker_ptr,
-    long delay,
-    long period,
-    long on_duration,
-    long count);
   int addFilteredNoisePwmAt(size_t frequency,
     double bandwidth,
     const ConstantString * const speaker_ptr,
@@ -102,12 +69,6 @@ public:
     long period,
     long on_duration,
     long count);
-  int startFilteredNoisePwm(size_t frequency,
-    double bandwidth,
-    const ConstantString * const speaker_ptr,
-    long delay,
-    long period,
-    long on_duration);
   int startFilteredNoisePwmAt(size_t frequency,
     double bandwidth,
     const ConstantString * const speaker_ptr,
@@ -138,49 +99,10 @@ private:
 
   void enableAudioCodec();
   void updatePlaying();
-  void addDirectoryToResponse(File dir,
-    const char * pwd);
   void setVolume(long volume);
   void setPlaying(bool playing);
 
   // Handlers
-  void setVolumeHandler();
-  void getAudioMemoryUsageHandler();
-  void getAudioMemoryUsageMaxHandler();
-  void resetAudioMemoryUsageMaxHandler();
-  void getAudioProcessorUsageHandler();
-  void getAudioProcessorUsageMaxHandler();
-  void resetAudioProcessorUsageMaxHandler();
-  void getSDCardInfoHandler();
-  void getAudioPathsHandler();
-  void playPathHandler();
-  void playToneHandler();
-  void playToneAtHandler();
-  void playNoiseHandler();
-  void playNoiseAtHandler();
-  void playFilteredNoiseHandler();
-  void playFilteredNoiseAtHandler();
-  void stopHandler();
-  void isPlayingHandler();
-  void getLastAudioPathPlayedHandler();
-  void getPositionHandler();
-  void getLengthHandler();
-  void getPercentCompleteHandler();
-  void addTonePwmHandler();
-  void addTonePwmAtHandler();
-  void startTonePwmHandler();
-  void startTonePwmAtHandler();
-  void addNoisePwmHandler();
-  void addNoisePwmAtHandler();
-  void startNoisePwmHandler();
-  void startNoisePwmAtHandler();
-  void addFilteredNoisePwmHandler();
-  void addFilteredNoisePwmAtHandler();
-  void startFilteredNoisePwmHandler();
-  void startFilteredNoisePwmAtHandler();
-  void stopPwmHandler();
-  void stopAllPwmHandler();
-  void isPulsingHandler();
   void playToneHandler(int index);
   void playNoiseHandler(int index);
   void playFilteredNoiseHandler(int index);
